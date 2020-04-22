2bfa0e3c9f23eb333d1e4d26312a6e4620ef88e7
chris <caconstan@yahoo.com>
2020-04-22 16:28:47 -0400

    Wed EOD commit

---
+++ Lab 6 Powers Table/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab_6_Powers_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int userInteger = ReadIntegers("Enter an integer: ");
+
+                Console.WriteLine("Number\tSquared\tCubed");
+                Console.WriteLine("======\t=======\t=====");
+
+                for (int i=1; i <= userInteger; i++)
+                {
+                    Console.WriteLine($"{Math.Pow(i, 1)}\t{Math.Pow(i, 2)}\t{Math.Pow(i, 3)}");
+                }
+            } while (RunAgain());
+        }
+        static int ReadIntegers(string prompt)
+        {
+            try
+            {
+                Console.WriteLine(prompt);
+                return int.Parse(Console.ReadLine().Trim());
+            }
+            catch (Exception myEX)
+            {
+                Console.WriteLine("I'm sorry, I didn't understand.");
+                return ReadIntegers(prompt);
+            }
+        }
+
+        static bool RunAgain()
+        {
+            Console.WriteLine("Continue? (y/n)");
+            string response = Console.ReadLine().Trim();
+            if (response == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
